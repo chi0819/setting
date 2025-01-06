2b88b32ea4c769e4dab144a34ed5305ebec0c890
tzuchilin <eric1231.tw@gmail.com>
2025-01-06 13:19:29 +0800

    install python3.12 in pyenv

--- linux/linux-setting.sh
+++ linux/linux-setting.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Ask sudo permission
+# Ask sudo Permission
 echo "Enter sudo password: "
 sudo -v
 
-# Keep sudo permission valid
+# Keep sudo Permission Valid
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Update & upgrade packages manager
+# Update & Upgrade Packages Manager
 sudo apt update
 sudo apt upgrade
 
-# Install tools by apt
+# Install Tools by apt
 sudo apt install -y verilator tree make cmake vim wget gpg apt-transport-https clang build-essential curl
 
-# Configure git
+# Configure Git
 git config --global user.email "eric1231.tw@gmail.com"
 git config --global user.name "tzuchilin"
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
-# Install vim plugin
+# Install Vim Plugin
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install OpenJDK
@@ -28,11 +28,11 @@ echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_COD
 sudo apt update
 sudo apt install -y temurin-21-jdk
 
-# Install mill
+# Install Mill
 curl -L https://github.com/com-lihaoyi/mill/releases/download/0.10.15/0.10.15 > mill && chmod +x mill
 sudo mv mill /usr/local/bin
 
-# Install firtool
+# Install Firtool
 wget https://github.com/llvm/circt/releases/download/firtool-1.95.1/firrtl-bin-linux-x64.tar.gz
 tar -xvzf firrtl-bin-linux-x64.tar.gz
 sudo mv firtool-1.95.1/bin/firtool /usr/local/bin
@@ -43,8 +43,9 @@ sudo apt install -y libssl-dev zlib1g-dev libbz2-dev  \
   libreadline-dev libsqlite3-dev llvm libncurses5-dev \
   libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl
 curl https://pyenv.run | bash
+pyenv install 3.12.0
 
-# Setting environment
+# Setting Environment
 sudo chmod 700 linux-relinking.sh
 bash linux-relinking.sh
 
